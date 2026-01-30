8e32cb29a98060a011348b54efbd99d810dd528b
vishwa <vcl.com>
2026-01-30 20:07:07 +0000

    updated SG

--- main.tf
+++ main.tf
@@ -6,10 +6,17 @@ module "Vms_app1" {
   subnets = ["subnet-084785ce11bb38827", "subnet-0e100c7e306c1812d"]
   num_of_vm = 1
   env = "dev"
+  web_sg = [module.Web_SG01.web_sg_id]
 
 }
 
-output "vm_private_ips01" {
-  value = module.Vms_app1.vm_priv_pips
+module "Web_SG01" {
+  source = "git::https://github.com/Vishwanathms/tf-aws-modules-jan26-w4.git//security-groups?ref=main"
+  project = "vishwa"
+  vpc_id = data.aws_vpc.default.id
+}
+
+data "aws_vpc" "default" {
+  default = true
 }
 
